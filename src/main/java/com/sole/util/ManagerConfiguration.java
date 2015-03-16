a3a9e72e626b7afdeae5df32f4ec4f4662d9c621
anxin323 <276284755@qq.com>
2015-03-16 14:59:06 +0800

    新增文件

---
+++ src/main/java/com/sole/util/ManagerConfiguration.java
@@ -0,0 +1,28 @@
+package com.sole.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ManagerConfiguration {
+	 @Value("${esserver.cluster.name}")  
+	    private String clusterName;  
+	  
+	    @Value("${esserver.path.home}")  
+	    private String pathHome;  
+	  
+	    @Value("${esserver.path.data}")  
+	    private String pathData;  
+	  
+	    public String getClusterName() {  
+	        return clusterName;  
+	    }  
+	  
+	    public String getPathData() {  
+	        return pathData;  
+	    }  
+	  
+	    public String getPathHome() {  
+	        return pathHome;  
+	    }  
+}
